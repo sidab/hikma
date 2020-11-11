36bac05cb6ae23de6965f8e70079163e7339a02c
sidab <info@sidab.ru>
2020-11-11 13:15:40 +0300

    fix

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -51,14 +51,6 @@ module.exports = [
         "runs": true
     },
     {
-        "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
-        "id": "onesignal-cordova-plugin.OneSignal",
-        "pluginId": "onesignal-cordova-plugin",
-        "clobbers": [
-            "OneSignal"
-        ]
-    },
-    {
         "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
         "id": "cordova-plugin-camera.Camera",
         "pluginId": "cordova-plugin-camera",
@@ -326,6 +318,14 @@ module.exports = [
         "clobbers": [
             "AudioRecorder"
         ]
+    },
+    {
+        "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
+        "id": "onesignal-cordova-plugin.OneSignal",
+        "pluginId": "onesignal-cordova-plugin",
+        "clobbers": [
+            "OneSignal"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -338,7 +338,6 @@ module.exports.metadata =
     "cordova-plugin-splashscreen": "6.0.0",
     "cordova-plugin-statusbar": "2.4.3",
     "cordova-plugin-whitelist": "1.3.4",
-    "onesignal-cordova-plugin": "2.11.1",
     "cordova-plugin-camera": "5.0.1",
     "cordova-plugin-file": "6.0.2",
     "cordova-plugin-media": "5.0.3",
@@ -346,7 +345,8 @@ module.exports.metadata =
     "cordova-plugin-x-socialsharing": "5.4.6",
     "phonegap-plugin-barcodescanner": "8.1.0",
     "com.mljsgto222.cordova.plugin.audiorecorder": "0.7.1",
-    "cordova-plugin-geolocation": "4.0.2"
+    "cordova-plugin-geolocation": "4.0.2",
+    "onesignal-cordova-plugin": "2.11.1"
 }
 // BOTTOM OF METADATA
 });
